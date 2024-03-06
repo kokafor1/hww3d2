b9fb04d11e542a2b16182a129170e35ed62692be
kokafor1 <kennedyokafor2003@gmail.com>
2024-03-05 22:49:46 -0500

    hw

---
+++ geometry.py
@@ -0,0 +1,8 @@
+#find out about this math import from the net for pi
+import math
+
+def circle_area(radius):
+    return math.pi * radius**2
+
+def hypotenuse(side1, side2):
+    return math.sqrt(side1**2 + side2**2)
